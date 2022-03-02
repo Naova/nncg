fd7bf6adeb0d41f5c323fab9bd7552b2dc2c4e3e
Florent Duchesne <florentduchesne@hotmail.com>
2022-03-02 20:51:01 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='NNCG',
+      version='1.0',
+      description='Neural Network Code Generator',
+      packages=['nncg', 'nncg.nodes', 'nncg.traverse', 'nncg.traverse.actions'],
+)
